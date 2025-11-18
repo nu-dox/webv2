3a2cdf47caade477c2a5d2b5adfc0d1112ab2695
Mikita Slabysh <slabysh2015@gmail.com>
2025-11-17 21:18:58 -0500

    Small changes

--- src/lib/SearchResult.svelte
+++ src/lib/SearchResult.svelte
@@ -3,7 +3,7 @@
   export let description: string = 'No description available';
 </script>
 
-<div class="p-4 border rounded-lg shadow hover:shadow-lg transition-shadow bg-gray-600">
+<div class="p-4 border rounded-lg shadow hover:shadow-lg transition-shadow bg-gray-600 hover:bg-gray-500 cursor-pointer">
   <h2 class="text-lg font-semibold mb-2">{name}</h2>
   <p class="text-sm text-white">{description}</p>
 </div>
