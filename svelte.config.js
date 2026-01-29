3abea2c7bc3d5b3812f6d30d3f72642aa0d5ebdc
Mikita Slabysh <slabysh2015@gmail.com>
2026-01-29 17:23:04 -0500

    Fixed svelte and tsconfig, broke terminus data pull SUKA BLYAT

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	preprocess: [sveltePreprocess(), mdsvex()],
+	kit: {
+		adapter: adapter() 
+	},
 	extensions: ['.svelte', '.svx']
 };
 
